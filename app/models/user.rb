b6245988d650e4f03c91e8eb1903c2913d97eb79
mitch olivier <m-olivier@olivier.localb2o.com>
2020-03-30 14:27:44 +0200

    Population User and Post Table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  include DeviseTokenAuth::Concerns::User
+	has_many :posts
+
+	before_save { self.email = email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+	validates :email, presence: true, 
+				format: { with: VALID_EMAIL_REGEX }, 
+				uniqueness: { case_sensitive: false } 
+	validates :name, presence: true, length: { maximum: 50 } 
+
+	devise :database_authenticatable, :registerable,
+	       :recoverable, :rememberable, :trackable, :validatable
+	include DeviseTokenAuth::Concerns::User
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do
+  user = User.create!(
+  	name: Faker::Name.first_name, 
+  	nickname: Faker::Name.last_name,
+  	email: Faker::Internet.email,
+  	password: Faker::Internet.password)
+  3.times do
+    user.posts.create!(title: Faker::Lorem.sentence(word_count = 3), body: Faker::Lorem::paragraph(sentence_count = 3))
+  end
+end
